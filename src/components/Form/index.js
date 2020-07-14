17cd9ff91e727e05dbe3a97bf346fa49c634751f
KhadijeElzein <khady_elzein@hotmail.com>
2020-07-14 18:38:04 -0300

    interface de números complexos

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,11 +1,12 @@
-import React, { useState } from  'react';
+import React, {useState,useRef} from  'react';
 import {InputText} from 'primereact/inputtext';
-import {InputNumber} from 'primereact/inputnumber';
 import {Button} from 'primereact/button';
 import {useDispatch, useSelector} from 'react-redux';
 import {actions} from '../../actions';
+import {Messages} from 'primereact/messages';
 
 const Form = () => {
+    let messages = useRef(null);
     const {resultado} = useSelector(store => store.reducer);
     const dispatch = useDispatch();
     const [realn1, setRealn1] = useState('');
@@ -29,24 +30,45 @@ const Form = () => {
     };
 
     const setFiltro = () => {
-        let filter = {}
-        
-        if(realn1!== undefined && realn1!==null)
+        let filter = {};
+        let msg = [];
+        if(realn1!== undefined && realn1!==null){
+            if(isNaN(realn1)){
+                msg.push({severity: 'error', summary: 'Erro', detail: 'Parte real do número 1 deve ser um número real'});
+                filter["erro"] = true;
+            }
             filter["realn1"] = realn1;
-        if(imagn1!== undefined && imagn1!==null)
+        }
+        if(imagn1!== undefined && imagn1!==null){
+            if(isNaN(imagn1)){
+                msg.push({severity: 'error', summary: 'Erro', detail: 'Parte imaginária do número 1 deve ser um número real'});
+                filter["erro"] = true;
+            }
             filter["imagn1"] = imagn1;
-        if(realn2!== undefined && realn2!==null)
+        }
+        if(realn2!== undefined && realn2!==null){
+            if(isNaN(realn2)){
+                msg.push({severity: 'error', summary: 'Erro', detail: 'Parte real do número 2 deve ser um número real'});
+                filter["erro"] = true;
+            }
             filter["realn2"] = realn2;
+        }
         if(imagn2!== undefined && imagn2!==null)
+            if(isNaN(imagn2)){
+                msg.push({severity: 'error', summary: 'Erro', detail: 'Parte imaginária do número 2 deve ser um número real'});
+                filter["erro"] = true;
+            }
             filter["imagn2"] = imagn2;
+        messages.current.show(msg);
         return filter;
         };
     const onAdicionar = () => {
         let filter = setFiltro();
-        dispatch({
-            type: actions.ADICIONAR_NUMERO_REQUESTED,
-            payload: filter,
-        });
+        if(filter.erro === undefined)
+            dispatch({
+                type: actions.ADICIONAR_NUMERO_REQUESTED,
+                payload: filter,
+            });
     };
     const onMultiplicar = () => {
         let filter = setFiltro();
@@ -57,30 +79,30 @@ const Form = () => {
     };
     return (
         <>
-
+        <Messages ref={messages} />
         <div className='p-grid'>
             <div className='p-col-12 p-md-6 p-lg-6'>
                 <label>Parte Real número 1</label>
                 <span className='p-fluid'>
-                    <InputNumber value = {realn1} onChange = {inputHandler1}/>
+                    <InputText value = {realn1} onChange = {inputHandler1}/>
                 </span>
             </div>
             <div className='p-col-12 p-md-6 p-lg-6'>
                 <label>Parte Imaginária número 1</label>
                 <span className='p-fluid'>
-                    <InputNumber value = {imagn1}  onChange = {inputHandler2} />
+                    <InputText value = {imagn1}  onChange = {inputHandler2} />
                 </span>
             </div>
             <div className='p-col-12 p-md-6 p-lg-6'>
                 <label>Parte Real número 2 </label>
                 <span className='p-fluid'>
-                    <InputNumber value = {realn2} onChange = {inputHandler3} />
+                    <InputText value = {realn2} onChange = {inputHandler3} />
                 </span>
             </div>
             <div className='p-col-12 p-md-6 p-lg-6'>
                 <label>Parte Imaginária número 2</label>
                 <span className='p-fluid'>
-                    <InputNumber value = {imagn2} onChange = {inputHandler4} />
+                    <InputText value = {imagn2} onChange = {inputHandler4} />
                 </span>
             </div>
         </div>
